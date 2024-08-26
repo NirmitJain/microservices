43ed682605b8b48400909706b988da4da033e2cc
NirmitJain <nirmit.jain@gmail.com>
2024-08-26 17:14:37 +0530

    Resiliency code commit

---
+++ section10-Resiliency/cards/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS cards(
+ card_id int NOT NULL AUTO_INCREMENT,
+ mobile_number varchar(10) NOT NULL,
+ card_number varchar(16) NOT NULL,
+ card_type varchar(20) NOT NULL,
+ total_limits int NOT NULL,
+ amount_used int NOT NULL,
+ available_amount int NOT NULL,
+ created_at date NOT NULL,
+ created_by varchar(25) NOT NULL,
+ updated_by varchar(25) DEFAULT NULL,
+ updated_at Date DEFAULT NULL,
+ PRIMARY KEY(card_id)
+);
